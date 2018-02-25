173f077f007a10415c6468e14a5a5d8c372d9fd1
christopherchristensen <christopher.christensen@live.com>
2018-02-25 18:40:16 +0100

    stat serie_01.2.py done

--- stat/serie_01.2.py
+++ stat/serie_01.2.py
@@ -6,3 +6,23 @@ Created on Sun Feb 25 18:09:18 2018
 @author: Christopher
 """
 
+from pandas import Series,DataFrame 
+import pandas as pd
+
+data = pd.read_csv(r"child.csv", sep=",", index_col=0)
+
+data.shape
+data.describe()
+data.index
+
+print("01.2 d.) : China existiert nicht und die Spaltennamen erhält man als Array")
+print("")
+
+data.sort_values(by="Drunkenness", ascending=0)[0:5]["Drunkenness"]
+
+data.nsmallest(1,"Infant.mortality")["Infant.mortality"]
+
+physical_act = data["Physical.activity"]
+mean_act     = physical_act.mean()
+
+data.loc[physical_act < mean_act, :].index
---
+++ stat/serie_01.3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 25 17:52:43 2018
+
+@author: Christopher
+"""
+
