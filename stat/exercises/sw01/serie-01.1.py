393dc2d0d2768e404490a46ba9f64eb86ceb0775
Christopher Christensen <christopher.christensen@live.com>
2018-09-20 15:24:28 +0200

    solve series-01

--- stat/exercises/sw01/serie-01.1.py
+++ stat/exercises/sw01/serie-01.1.py
@@ -3,16 +3,17 @@
 """
 Created on Sat Sep  8 12:48:49 2018
 
-@author: christopher
+@author: christopher j. christensen
 """
 
 # AUFGABE 1.1
 
 ##################################################
 
-print("Hello World!")
+print("Hello STAT-Friends!")
 
 ##################################################
+## Einführung Python
 
 a = 3
 b = 4
@@ -76,8 +77,71 @@ temp = DataFrame({
         index=["jan", "feb", "mar", "apr", "may", "jun", "jul"]
 )
 
+# Dataframe ausgeben
 temp
 
+# Spaltennamen abfragen
 temp.columns
 
+# Durchschnittliche Temperaturen in den einzelnen Städten
 temp.mean()
+
+# Durchschnittliche Temperaturen in den einzelnen Monaten aller Städte
+temp.mean(axis=1)
+
+# Mindesttemperaturen der einzelnen Monaten
+temp.min(axis=1)
+
+# Eine einzelne Spalte betrachten
+temp["Luzern"]
+
+# Selbe Abfrage wie oben einfach in Matrizenform
+# Der alleinstehende : als 1. Argument soll andeuten, 
+# dass alle Zeilen gemeint sind.
+# Der : als 1. Argument hat die Bedeutung „von ... bis”
+temp.loc[:, "Luzern"]
+
+# Temperaturen von Mai bis Juli in Luzern
+temp.loc["may":"jul", "Luzern"]
+
+# Auswahl von Zeilen und Spalten
+temp.loc[["may", "jan"], ["Luzern", "Basel"]]
+
+# Einzelner Wert abfragen
+temp.loc["may", "Luzern"]
+
+
+##################################################
+
+# AUFGABE 1.2
+
+##################################################
+
+from pandas import Series,DataFrame 
+import pandas as pd
+
+# Datei einlesen und in Variable (data) speichern
+data = pd.read_csv(r"./child.csv", sep=",", index_col=0)
+
+## TODO...
+
+##################################################
+
+# AUFGABE 1.3
+
+##################################################
+
+import pandas as pd
+from pandas import DataFrame, Series
+fuel = pd.read_table(r"d.fuel.dat", sep=",", index_col=0)
+
+
+##################################################
+
+# AUFGABE 1.4
+
+##################################################
+
+import pandas as pd
+from pandas import DataFrame, Series
+fuel = pd.read_table(r"*d.fuel.dat", sep=",", index_col=0)
