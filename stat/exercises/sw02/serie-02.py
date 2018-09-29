0085af0be6b31ab80e0e2b98ab71ccfda697233f
Christopher Christensen <christopher.christensen@live.com>
2018-09-29 11:06:23 +0200

    stat exercise sw02 done

--- stat/exercises/sw02/serie-02.py
+++ stat/exercises/sw02/serie-02.py
@@ -82,22 +82,22 @@ x
 # --- a.
 
 # Unveränderter Datensatz
-income1 = Series([
+grades1 = Series([
             4.2, 2.3, 5.6, 4.5, 4.8, 3.9, 5.9, 2.4, 5.9, 6, 4, 3.7, 5, 5.2, 
             4.5, 3.6, 5, 6, 2.8, 3.3, 5.5, 4.2, 4.9, 5.1
         ]);
 
-income1.mean() # 4.5125
-income1.median() # 4.65
+grades1.mean() # 4.5125
+grades1.median() # 4.65
 
 # Veränderter Datensatz
-income2 = Series([
+grades2 = Series([
             2.0, 1.0, 5.7, 4.5, 4.8, 3.9, 5.9, 2.4, 5.9, 8, 4, 3.7, 5, 5.2, 
             4.5, 3.6, 5, 8, 0.2, 2.3, 5.5, 4.2, 4.9, 5.1
         ]);
 
-income2.mean() # 4.3875
-income2.median() # 4.65
+grades2.mean() # 4.3875
+grades2.median() # 4.65
 
 # ---
 
@@ -105,19 +105,19 @@ income2.median() # 4.65
 
 # Histogramm unveränderter Datensatz
 plt.subplot(221)
-income1.plot(kind="hist", bins=24, edgecolor="black")
+grades1.plot(kind="hist", bins=24, edgecolor="black")
 
 # Boxplot unveränderter Datensatz
 plt.subplot(222)
-income1.plot(kind="box", title="Income1")
+grades1.plot(kind="box", title="Grades unchanged")
 
 # Histogramm veränderter Datensatz
 plt.subplot(223)
-income2.plot(kind="hist", bins=24, edgecolor="black")
+grades2.plot(kind="hist", bins=24, edgecolor="black")
 
 # Boxplot veränderter Datensatz
 plt.subplot(224)
-income2.plot(kind="box", title="Income2")
+grades2.plot(kind="box", title="Grades changed")
 
 plt.subplots_adjust(wspace = 0.5, hspace=0.5)
 
@@ -177,6 +177,24 @@ schlamm["Pr9"].plot(kind="box")
 schlamm.mean()
 schlamm.median()
 
+
+##################################################
+
+# AUFGABE 2.3
+
+##################################################
+
+
+# a = 3
+# b = 1
+# c = 2
+
+# Einzeichnen des Medians in die Verteilungsfunktion
+# hilft dabei zu sehen welcher Median am meisten dem
+# eines Boxplots gleicht. Danach kontrollieren ob 
+# die Streuung auch zutrifft (rechts-/linksschief)
+
+
 ##################################################
 
 # AUFGABE 2.4
@@ -188,10 +206,217 @@ hubble = pd.read_table(r"hubble.txt", sep=" ")
 hubble.describe
 hubble.head()
 
-# Koeffizienten B0 und B1
+# Koeffizienten B0 und B1 berechnen
 b, a = np.polyfit(hubble["distance"], hubble["recession.velocity"], deg=1)
-x = np.linspace(hubble["distance"].min() ,hubble["recession.velocity"].max())
+b, a
 
+# Vektor erzeugen mit Minimum von distance 
+# als 1. Wert und Maximum als 2. Wert
+z = np.linspace(hubble["distance"].min() ,hubble["distance"].max())
+
+# Streudiagramm (Scatterplot) erzeugen aus den Hubble-Daten
 hubble.plot(kind="scatter", x="distance", y="recession.velocity")
-plt.plot(x, a+b*x, c="orange")
+
+# Regressionsgerade aus den Koeffizienten (a und b) 
+# und dem Vektor (z) des Anfangs- und Endpunktes
+plt.plot(z, a + (b * z), c="orange")
+
+# Plotten
 plt.show()
+
+# Korrelationskoeffizienten bestimmen
+hubble.corr().iloc[0,1] # 0.789639
+
+# Je näher die Werte zu +/- 1 stehen, desto grösser ist
+# Korrelation (geniessen mit Vorsicht!).
+# Da 0.789639 ziemlich nahe zu +1 steht könnte man 
+# argumentieren, dass es eine Korrelation zwischen der 
+# distance und der recession.velocity gibt.
+
+
+##################################################
+
+# AUFGABE 2.5
+
+##################################################
+
+
+# --- a.
+# Daten einlesen
+income = pd.read_table(r"income.dat", sep=" ")
+
+# Daten beschreiben
+income.head()
+income.describe
+
+# Streudiagramm Schulbildung versus Einkommen
+income.plot(kind="scatter", x="Educ", y="Income2005")
+plt.show()
+
+# Streudiagramm Intelligenzquotient versus Einkommen
+income.plot(kind="scatter", x="AFQT", y="Income2005")
+plt.show()
+# ---
+
+# --- b.
+# Plot (Bildung/Einkommen)
+income.plot(kind="scatter", x="Educ", y="Income2005")
+
+# Koeffizienten B0 und B1 berechnen (Bildung/Einkommen)
+b, a = np.polyfit(x=income["Educ"], y=income["Income2005"], deg=1)
+
+# Vektor mit Minimum- und Maximumwert (Bildung)
+z = np.linspace(income["Educ"].min(), income["Educ"].max())
+
+# Regressionsgerade auf Plot projezieren (Bildung/Einkommen)
+plt.plot(z, a + (b * z), c="orange")
+
+# Streudiagramm plotten
+plt.show()
+
+
+# Plot (IQ/Einkommen)
+income.plot(kind="scatter", x="AFQT", y="Income2005")
+
+# Koeffizienten B0 und B1 berechnen (IQ/Einkommen)
+b, a = np.polyfit(x=income["AFQT"], y=income["Income2005"], deg=1)
+
+# Vektor mit Minimum- und Maximumwert (IQ)
+z = np.linspace(income["AFQT"].min(), income["AFQT"].max())
+
+# Regressionsgerade auf Plot projezieren (IQ/Einkommen)
+plt.plot(z, a + (b * z), c="orange")
+
+# Streudiagramm plotten
+plt.show()
+
+# Rein von den Diagrammen her hat die Regressionsgerade
+# bei beiden Diagrammen keine starke Steigung. Dies
+# weist eher auf wenig Korrelation hin.
+# ---
+
+# --- c.
+# Korrelation zwischen Schulbildung und Einkommen
+income.corr().loc["Educ", "Income2005"] # 0.34565
+
+# Dies weist ebenfalls auf sehr wenig Korrelation hin.
+# Aus diesem Grund ist ein Regressionsmodell für diesen
+# Datensatz eher nicht angebracht.
+# ---
+
+
+##################################################
+
+# AUFGABE 2.6
+
+##################################################
+
+
+# Daten erstellen
+x = np.array([10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5])
+
+y1 = np.array([8.04, 6.95, 7.58, 8.81, 8.33, 9.96, 
+               7.24, 4.26, 10.84, 4.82, 5.68]) 
+
+y2 = np.array([9.14, 8.14, 8.74, 8.77, 9.26, 8.10, 
+               6.13, 3.10, 9.13, 7.26, 4.74]) 
+
+y3 = np.array([7.46, 6.77, 12.74, 7.11, 7.81, 8.84, 
+               6.08, 5.39, 8.15, 6.42, 5.73]) 
+
+x4 = np.array([8, 8, 8, 8, 8, 8, 8, 19, 8, 8, 8])
+
+y4 = np.array([6.58, 5.76, 7.71, 8.84, 8.47, 7.04, 
+               5.25, 12.50, 5.56, 7.91, 6.89])
+
+# --- a.
+# Datensatz 1
+plt.subplot(221)
+plt.scatter(x, y1)
+
+# Koeffizienten B0, B1 (Datensatz 1)
+b, a = np.polyfit(x=x, y=y1, deg=1)
+
+# Vektor mit Anfangs- und Endwert (Datensatz 1)
+z = np.linspace(x.min(), x.max())
+
+# Regressionsgerade hinzufügen (Datensatz 1)
+plt.plot(z, a + (b * z), c="orange")
+
+# Datensatz 2
+plt.subplot(222)
+plt.scatter(x, y2)
+
+# Koeffizienten B0 und B1 (Datensatz 2)
+b, a = np.polyfit(x=x, y=y2, deg=1)
+
+# Vektor mit Anfangs- und Endwert (Datensatz 2)
+z = np.linspace(x.min(), x.max())
+
+# Regressionsgerade hinzufügen (Datensatz 2)
+plt.plot(z, a + (b * z), c="orange")
+
+# Datensatz 3
+plt.subplot(223)
+plt.scatter(x, y3)
+
+# Koeffizienten B0 und B1 (Datensatz 3)
+b, a = np.polyfit(x=x, y=y3, deg=1)
+
+# Vektor mit Anfangs- und Endwert (Datensatz 3)
+z = np.linspace(x.min(), x.max())
+
+# Regressionsgerade hinzufügen (Datensatz 3)
+plt.plot(z, a + (b * z), c="orange")
+
+# Datensatz 4
+plt.subplot(224)
+plt.scatter(x4, y4)
+
+# Koeffizienten B0 und B1 (Datensatz 4)
+b, a = np.polyfit(x=x4, y=y4, deg=1)
+
+# Vektor mit Anfangs- und Endwert (Datensatz 4)
+z = np.linspace(x4.min(), x4.max())
+
+# Regressionsgerade hinzufügen (Datensatz 4)
+plt.plot(z, a + (b * z), c="orange")
+
+# Abstände zwischen Subplots definieren
+plt.subplots_adjust(wspace = 0.5, hspace=0.5)
+
+plt.show()
+
+# Datensatz 1: Es besteht Grund zur Annahme einer Korrelation. 
+#              (steigende Regressionsgerade)
+
+# Datensatz 2: Obwohl die Regressionsgerade steigt, wäre es hier
+#              unangebracht sich auf eine bestehende Korrelation 
+#              festzulegen. Die Verteilung zeigt ein Muster jedoch
+#              jedoch keines, dass auf eine eindeutige Korrelation
+#              hinweisen könnte.
+
+# Datensatz 3: Dieser Datensatz weist eher eine Korrelation auf
+#              mit Ausnahme eines Ausreissers. 
+#              (steigende Regressionsgerade)
+
+# Datensatz 4: Auch hier sehen wir eine steigende Regressionsgerade,
+#              jedoch sieht man eindeutig, dass keine Korrelation
+#              existiert. Die Regressionsgerade ist steigend, jedoch
+#              nur Dank eines Ausreissers.
+# ---
+
+# --- b.
+# Koeffizienten berechnen aller Datensätze
+b1, a1 = np.polyfit(x=x, y=y1, deg=1)  # a1 = 3.0000909, b1 = 0.5000909
+b2, a2 = np.polyfit(x=x, y=y2, deg=1)  # a2 = 3.0009091, b2 = 0.5000000
+b3, a3 = np.polyfit(x=x, y=y3, deg=1)  # a3 = 3.0024545, b3 = 0.4997273
+b4, a4 = np.polyfit(x=x4, y=y4, deg=1) # a4 = 3.0017273, b4 = 0.4999091
+# ---
+
+# --- c.
+c1 = np.corrcoef(x, y1)[1][0]  # 0.8164205 (steigende Regressionsgerade)
+c2 = np.corrcoef(x, y2)[1][0]  # 0.8162365 (steigende Regressionsgerade)
+c3 = np.corrcoef(x, y3)[1][0]  # 0.8162867 (steigende Regressionsgerade)
+c4 = np.corrcoef(x4, y4)[1][0] # 0.8165214 (steigende Regressionsgerade)
+# ---
